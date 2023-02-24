6e95c7ddfe7e4cc9040125bd5d4a6123c4f4469a
Carson Black <carsontimothyblack@gmail.com>
2023-02-24 10:59:16 -0500

    Declaring Ruby variables.

---
+++ learn-to-code-with-ruby/03 - Variables/declaring_variables.rb
@@ -0,0 +1,24 @@
+age = 40
+puts age
+
+name = "Carson"
+last_name = "Black"
+puts name
+puts last_name
+
+puts age + 7 # 40 + 7
+puts name + last_name
+
+age = 41
+puts age
+
+age_in_ten_years = age + 10
+puts age_in_ten_years
+
+age = age + 7
+puts age
+
+chameleon = 24
+chameleon = "Some random text"
+chameleon = 3.14
+puts chameleon
