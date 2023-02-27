c2162a1b34ee411b98fb50c1706ce9e4b74ed0ae
Carson Black <carsontimothyblack@gmail.com>
2023-02-27 14:57:09 -0500

    Parameters and Arguments in Ruby.

---
+++ learn-to-code-with-ruby/08 - Methods I/parameters_and_arguments.rb
@@ -0,0 +1,11 @@
+def praise_person(name)
+    puts "#{name} is amazing"
+    puts "#{name} is charming"
+    puts "#{name} is talented"
+end
+
+praise_person("Carson")
+praise_person("Bobby")
+praise_person "Debbie"
+# praise_person
+# praise_person("Debbie", "Ben")
