7cbe20ab82e8db10db7b9e4c92abb218eba61683
Carson Black <carsontimothyblack@gmail.com>
2023-02-27 14:59:03 -0500

    Multiple parameters in Ruby.

---
+++ learn-to-code-with-ruby/08 - Methods I/multiple_parameters.rb
@@ -0,0 +1,10 @@
+def praise_person(name, age)
+    puts "#{name} is amazing"
+    puts "#{name} is charming"
+    puts "#{name} is talented"
+    puts "#{age} is how old #{name} is"
+end
+
+praise_person("Carson", "40")
+praise_person("Bobby", "50")
+praise_person "Debbie", "20"
