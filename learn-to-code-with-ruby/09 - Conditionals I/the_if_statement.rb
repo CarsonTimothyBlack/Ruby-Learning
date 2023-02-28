37d8cad36e236152689e27aface6316a30df03cd
Carson Black <carsontimothyblack@gmail.com>
2023-02-28 17:32:24 -0500

    Intro to conditionals in Ruby.

---
+++ learn-to-code-with-ruby/09 - Conditionals I/the_if_statement.rb
@@ -0,0 +1,38 @@
+
+if 5 < 7
+    puts "That math statement is true!"
+end
+
+if 5 < 2
+    puts "That math statement is true!"
+end
+
+password = "topsecret"
+
+if password == "topsecret"
+    puts "Congrats, you've logged into our stystem."
+end
+
+if password == "mango"
+    puts "Congrats, you've logged into our stystem."
+end
+
+word = "kangaroo"
+
+if word.length == 8
+    puts "That word has 8 letters!"
+end
+
+if word.length == 7
+    puts "That word has 7 letters!"
+end
+
+new_word = "zebra"
+
+if new_word.include?("eb")
+    puts "The word contains eb"
+end
+
+if new_word.include?("i")
+    puts "The word contains i"
+end
