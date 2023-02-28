d352eb353813fb5c900590fe60145416dba8bdd9
Carson Black <carsontimothyblack@gmail.com>
2023-02-28 14:22:46 -0500

    Equality operators for strings Ruby.

---
+++ learn-to-code-with-ruby/07 - Strings/equality_inequality_operators_strings.rb
@@ -0,0 +1,17 @@
+a = "Hello"
+b = "hello"
+c = "Hello"
+
+p a == b
+p a == c
+p b == c
+
+p a != b
+p a != c
+p "Apple" < "Banana"
+p "hello" < "help"
+
+p "A" < "a"
+p "Z" < "a"
+
+p "Help" < "Banana"
