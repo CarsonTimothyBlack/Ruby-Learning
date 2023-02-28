72b3214bd14869e666f423f2818afd6c3160d24c
Carson Black <carsontimothyblack@gmail.com>
2023-02-28 16:07:18 -0500

    Case methods in Ruby.

---
+++ learn-to-code-with-ruby/07 - Strings/case_methods.rb
@@ -0,0 +1,13 @@
+puts "hello".capitalize
+puts "heLLo".capitalize
+puts "Hello".capitalize
+puts "hello World".capitalize.class
+
+puts "carson123".upcase
+
+lowercase = "I'm patient"
+uppercase = lowercase.upcase
+p uppercase
+
+p "CARSON!@#%".downcase
+p "caRSon".swapcase
