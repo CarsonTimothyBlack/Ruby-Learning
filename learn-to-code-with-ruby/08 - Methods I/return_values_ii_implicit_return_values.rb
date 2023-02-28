ea3ad1844969eb30d9ab97412af2876f3615b481
Carson Black <carsontimothyblack@gmail.com>
2023-02-27 22:00:40 -0500

    Implicit values in Ruby.

---
+++ learn-to-code-with-ruby/08 - Methods I/return_values_ii_implicit_return_values.rb
@@ -0,0 +1,17 @@
+def add_two_numbers(num1, num2)
+    # puts "OK, I'm solving your math problem"
+    num1 + num2
+end
+
+def nothing
+    puts "What will be the return value here?" # nil
+end
+
+# p nothing
+result = nothing
+p result
+p result.class
+
+puts add_two_numbers(3, 4)
+p nothing
+puts nothing
