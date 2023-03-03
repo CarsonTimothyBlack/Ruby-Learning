7dc351468aa4812c0b2d1c42ac17d5d4614f8706
Carson Black <carsontimothyblack@gmail.com>
2023-03-03 13:12:41 -0500

    Include method in Ruby.

---
+++ learn-to-code-with-ruby/12 - Ranges/check_if_a_value_is_in_a_range.rb
@@ -0,0 +1,9 @@
+half_alphabet = "a".."m"
+puts half_alphabet.include?("j")
+puts half_alphabet.include?("z")
+puts half_alphabet.include?("E")
+
+numbers = -14...79
+puts numbers.include?(79)
+
+p half_alphabet === "k"
