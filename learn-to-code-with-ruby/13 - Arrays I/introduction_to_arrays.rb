fe1e9b1cb6bdd1ad27cd32f33cc199f646625bd0
Carson Black <carsontimothyblack@gmail.com>
2023-03-03 14:03:56 -0500

    Further intro to arrays in Ruby.

--- learn-to-code-with-ruby/13 - Arrays I/introduction_to_arrays.rb
+++ learn-to-code-with-ruby/13 - Arrays I/introduction_to_arrays.rb
@@ -1,4 +1,22 @@
-p []
-p [].class
 
-numbers = []
+numbers = [4, 8, 15, 16, 23, 42]
+
+p numbers
+print numbers
+
+toys = ["Teddy bear", "Super Soaker", "board game"]
+
+puts
+
+p toys
+puts toys
+
+things = [4, true, "Hello", 10.99]
+
+p things
+
+students = [["Boris", 25, true],
+["Sally", 23, true],
+["Ingrid", 31, false]]
+
+puts students
