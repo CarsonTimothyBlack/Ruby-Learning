d7a43d56d5df90487f5742529e79289804535782
Carson Black <carsontimothyblack@gmail.com>
2023-03-13 12:02:06 -0400

    empty? and nil? methods in Ruby.

---
+++ learn-to-code-with-ruby/13 - Arrays I/the_empty?_and_nil?_methods.rb
@@ -0,0 +1,14 @@
+puts [1, 2, 3].empty?
+
+puts [].empty?
+
+puts [true, true].empty?
+puts [nil, nil].empty?
+
+puts [1, 2, 3].nil?
+puts 1.nil?
+puts [].nil?
+
+letters = ("a".."j").to_a
+character = letters[25]
+p character.nil?
