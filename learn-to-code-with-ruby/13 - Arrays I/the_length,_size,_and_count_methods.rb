e21d30d3dd659943d682ba65fe1d5d7d203b7504
Carson Black <carsontimothyblack@gmail.com>
2023-03-13 11:57:49 -0400

    The length, size, and count methods in Ruby.

---
+++ learn-to-code-with-ruby/13 - Arrays I/the_length,_size,_and_count_methods.rb
@@ -0,0 +1,8 @@
+p [1, 2, 3, 4, "Hello", nil].length
+p [1, 2, 3, 4, "Hello", nil].size
+
+p [].length
+
+p [1, 2, 3, 4, 2, 3, 2, nil].count(2)
+
+p [true, true, false, true, false, false, false, "False"].count(true)
