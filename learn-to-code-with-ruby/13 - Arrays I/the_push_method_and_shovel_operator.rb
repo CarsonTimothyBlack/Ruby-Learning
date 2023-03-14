6c5560772a017794e5a703a9bd1219fb46d92280
Carson Black <carsontimothyblack@gmail.com>
2023-03-13 21:52:26 -0400

    push/insert methods and shovel operator in Ruby.

---
+++ learn-to-code-with-ruby/13 - Arrays I/the_push_method_and_shovel_operator.rb
@@ -0,0 +1,12 @@
+
+locations = ["House", "Airport", "Bar"]
+p locations
+
+locations.push("Restaurant", "Saloon")
+p locations
+
+locations << "China" << "Berlin"
+p locations
+
+locations.insert(1, "Gym", "Cafe")
+p locations
