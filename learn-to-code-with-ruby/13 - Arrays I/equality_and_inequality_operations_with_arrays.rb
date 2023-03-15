5a1038346075136ae4fbd0eb30788241e04ddfeb
Carson Black <carsontimothyblack@gmail.com>
2023-03-14 21:35:26 -0400

    Equality and inequality operators in Ruby.

---
+++ learn-to-code-with-ruby/13 - Arrays I/equality_and_inequality_operations_with_arrays.rb
@@ -0,0 +1,16 @@
+a = [1, 2, 3]
+b = [1, 2, 3, 4]
+c = [3, 2, 1]
+d = [1, 2, 3]
+
+p a == b
+p a == c
+p a == d
+
+p a != b
+p a != d
+
+candy = ["Skittles", "Starbursts" , "Snickers"]
+candy2 = ["Skittles", "Starbursts" , "snickers"]
+
+p candy == candy2
