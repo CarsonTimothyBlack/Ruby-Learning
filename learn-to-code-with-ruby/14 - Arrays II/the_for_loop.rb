c0041ea8bcc96724c7018c52ad4f31b85e6bd936
Carson Black <carsontimothyblack@gmail.com>
2023-03-16 16:37:38 -0400

    for loop in Ruby

---
+++ learn-to-code-with-ruby/14 - Arrays II/the_for_loop.rb
@@ -0,0 +1,19 @@
+numbers = [3, 5, 7]
+
+# num = 10
+
+# numbers.each { |num| puts num }
+
+# p num
+
+for num in numbers
+    puts num
+end
+
+p num
+
+rng = 1..10
+
+rng.each do |rng_number|
+    puts rng_number
+end
