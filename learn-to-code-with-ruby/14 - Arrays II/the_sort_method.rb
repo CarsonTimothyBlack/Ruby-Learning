0263a57227c7b545e6efeaeb22e0a0320eb4173a
Carson Black <carsontimothyblack@gmail.com>
2023-03-17 16:55:19 -0400

    sort method on an array in Ruby

---
+++ learn-to-code-with-ruby/14 - Arrays II/the_sort_method.rb
@@ -0,0 +1,13 @@
+numbers = [5, 13, 1, -2, 8]
+words = ["caterpillar", "kangaroo", "apple", "Zebra"]
+p numbers
+p words
+
+p numbers.sort.reverse
+p words.sort.reverse
+
+numbers.sort!
+words.sort!
+
+p numbers
+p words
