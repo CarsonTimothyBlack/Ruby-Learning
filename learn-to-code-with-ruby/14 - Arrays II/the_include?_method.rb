47533422514854a9fc3869e7446f4d898e274827
Carson Black <carsontimothyblack@gmail.com>
2023-03-18 21:45:54 -0400

    include? method on arrays

---
+++ learn-to-code-with-ruby/14 - Arrays II/the_include?_method.rb
@@ -0,0 +1,11 @@
+p "Hello".include?("H")
+
+numbers = [2, 55, 78, 54]
+
+p numbers.include?(4)
+p numbers. include?(78)
+
+strings = ["Apple", "Grape", "Fun"]
+
+p strings.include?("grape")
+p strings. include?("Fun")
