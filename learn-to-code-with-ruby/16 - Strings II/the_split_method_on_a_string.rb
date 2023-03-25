257be024ccf08b76455fd822528eb9807fb945c4
Carson Black <carsontimothyblack@gmail.com>
2023-03-24 20:23:17 -0400

    Code challenge for longest word.

--- learn-to-code-with-ruby/16 - Strings II/the_split_method_on_a_string.rb
+++ learn-to-code-with-ruby/16 - Strings II/the_split_method_on_a_string.rb
@@ -1,5 +1,27 @@
 sentence = "Hi, my name is Boris. There are spaces here!"
 
-words = sentence.split(" ")
+# words = sentence.split(" ")
 
-words.each { |word| puts word.length}
+# longest = 0
+# largest_word = ""
+# sentence.split(" ").each { |word| 
+#     if word.length >= longest
+#         longest = word.length  
+#         largest_word = word
+#     end
+# }
+# puts largest_word
+
+def longest_word(sentence)
+    longest = 0
+    largest_word = ""
+    sentence.split(" ").each { |word| 
+        if word.length >= longest
+            longest = word.length  
+            largest_word = word
+        end
+    }
+    puts largest_word
+end
+
+longest_word("Bobby loves big scary kangaroos")
