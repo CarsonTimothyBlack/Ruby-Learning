472bd1b6ae9390c63e90462785038772400ce228
Carson Black <carsontimothyblack@gmail.com>
2023-03-28 11:12:44 -0400

    index and rindex methods in Ruby

---
+++ learn-to-code-with-ruby/16 - Strings II/the_index_and_rindex_methods.rb
@@ -0,0 +1,26 @@
+fact = "I am very handsome."
+
+p fact.index("h")
+p fact.index("z")
+
+p fact.index("e")
+
+p fact.index("am")
+
+p fact.index("e", 7)
+
+p fact.rindex("e")
+
+def custom_index(string, substring)
+    return nil unless string.include?(substring)
+    length = substring.length
+    string.chars.each_with_index do |char, index|
+        sequence = string[index, length]
+        return index if sequence == substring
+    end
+end
+
+p custom_index(fact, "I")
+p custom_index(fact, "h")
+p custom_index(fact, "z")
+p custom_index(fact, "am")
