493af2fc0d77c16dda426419ba76815161ba3ef5
Carson Black <carsontimothyblack@gmail.com>
2023-04-05 16:16:15 -0400

    flatten method in Ruby

---
+++ learn-to-code-with-ruby/15 - Arrays III/the_flatten_method.rb
@@ -0,0 +1,16 @@
+registrations = [
+    ["Bob", "Dan", "Jack"],
+    ["Rick", "Susan", "Molly"],
+    ["Pierce", "Sean", "George"]
+]
+
+a, b, c = registrations
+p a
+p b
+p c
+
+p registrations.flatten
+p registrations
+
+registrations.flatten!
+p registrations
