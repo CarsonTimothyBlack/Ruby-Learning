d2b84dd49548aba86252fb1c610ded62b593aa03
Carson Black <carsontimothyblack@gmail.com>
2023-04-22 20:52:19 -0400

    hash extraction in Ruby

---
+++ learn-to-code-with-ruby/18 - Hashes I/create_hash_and_extract_values_from_keys.rb
@@ -0,0 +1,11 @@
+nfl_roster = {"Tom Brady" => "New England  Patriots", "Tony Romo" => "Dallas Cowboys", "Rob Gronkowski" => "New England Patriots"}
+
+nba_roster = {"Cleveland Cavaliers" => ["LeBron James", "Kevin Love", "Kyrie Irving"], "Golden State Warriors" => ["Stephen Currey", "Klay Thompson", "Jimmy Massive"]}
+
+p nfl_roster["Tony Romo"]
+p nfl_roster["Rob Gronkowski"]
+
+p nba_roster["Cleveland Cavaliers"]
+p nba_roster["Cleveland Cavaliers"][1]
+
+p nfl_roster["Donald Duck"]
